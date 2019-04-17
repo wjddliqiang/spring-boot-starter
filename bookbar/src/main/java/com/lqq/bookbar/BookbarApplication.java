fe0384d538b41f66d4431667292e91dd4a836348
wjddliqiang <421132310@qq.com>
2019-04-17 23:11:29 +0800

    增加maven profile配置，用以切换不同环境的配置，并为打包jar文件做了部分改动

--- bookbar/src/main/java/com/lqq/bookbar/BookbarApplication.java
+++ bookbar/src/main/java/com/lqq/bookbar/BookbarApplication.java
@@ -2,20 +2,33 @@ package com.lqq.bookbar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.lqq.bookbar.controller.Person;
  
+/**
+   * 在启动类当中加上extends SpringBootServletInitializer并重写configure方法，这是为了打包springboot项目用的
+ * @author LQQ
+ *
+ */
 @SpringBootApplication
 @EnableConfigurationProperties({Person.class})
-public class BookbarApplication {
+public class BookbarApplication extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BookbarApplication.class, args);
 		
 	}
-	
-	
+
+	/**
+	   *  项目打包使用
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 	
 
 }
